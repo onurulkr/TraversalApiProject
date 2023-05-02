7eb68d427e71345f4b4b2cfcca5d93a119c29792
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-05-02 10:38:56 +0300

    Update WeatherForecastController.cs

--- TraversalApiProject/Controllers/WeatherForecastController.cs
+++ TraversalApiProject/Controllers/WeatherForecastController.cs
@@ -15,7 +15,7 @@ namespace TraversalApiProject.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+ 
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
